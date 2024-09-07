992d90ae78fa61d4163b5fc4e03386d0df52e212
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-07 23:43:27 +0200

    Initial fyne setup

---
+++ cmd/picow-led-gui/content.go
@@ -0,0 +1,11 @@
+package main
+
+import "fyne.io/fyne/v2/widget"
+
+type Content struct {
+	widget.BaseWidget
+}
+
+func NewContent() *Content {
+	return &Content{}
+}
--- cmd/picow-led-gui/main.go
+++ cmd/picow-led-gui/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fyne.io/fyne/v2/app"
+)
+
 func main() {
-	// TODO: Fyne GUI
+	a := app.New()
+	w := a.NewWindow("PicoW LED")
+
+	w.SetContent(NewContent())
+	w.ShowAndRun()
 }
