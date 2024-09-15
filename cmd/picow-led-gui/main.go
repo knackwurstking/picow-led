1248d35080d07f3ea6a8e7ad7d974397d0cd66d2
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-15 20:56:19 +0200

    Set log flags to zero and update content and theme

--- cmd/picow-led-gui/main.go
+++ cmd/picow-led-gui/main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
+func init() {
+	log.SetFlags(0)
+}
+
 func main() {
 	a := app.New()
-
-	a.Settings().SetTheme(&customTheme{})
+	a.Settings().SetTheme(newCustomTheme())
 
 	w := a.NewWindow("PicoW LED")
 
-	w.SetContent(NewContent())
-
-	// TODO: Need to handle android size, text not centered with this custom theme
+	w.SetContent(newContent())
 	w.Resize(fyne.NewSize(600, 800))
-
 	w.ShowAndRun()
 }
