4a00e3cebc9098249cb90cccc7695011a38e895c
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-28 12:43:38 +0200

    Add power button component

--- frontend/src/app/pages/devices-components/picow-device-item.js
+++ frontend/src/app/pages/devices-components/picow-device-item.js
@@ -22,7 +22,67 @@ export default class PicowDeviceItem extends HTMLElement {
              * @param {Device} device
              */
             set(device) {
-                // TODO: ...
+                this.root.device = device;
+
+                const list = this.root.querySelector(`li.is-card`);
+                list.setAttribute("data-server-addr", device.server.addr);
+
+                if (!!device.color) {
+                    this.root.style.setProperty(
+                        "--current-color",
+                        `rgb(${device.color[0] || 0}, ${
+                            device.color[1] || 0
+                        }, ${device.color[2] || 0})`
+                    );
+                }
+
+                // ------------ //
+                // Update Label //
+                // ------------ //
+
+                {
+                    let primary = device.server.name || "";
+                    let secondary = device.server.addr;
+                    if (!primary) {
+                        primary = device.server.addr;
+                        secondary = "&nbsp;";
+                    }
+
+                    /**
+                     * @type {import("ui").UILabel}
+                     */
+                    const label = this.root.querySelector(`ui-label`);
+                    label.ui.primary = primary;
+                    label.ui.secondary = secondary;
+                }
+
+                // ------------------- //
+                // Update Power Button //
+                // ------------------- //
+
+                {
+                    // TODO: Pass device data to the power button
+                }
+
+                // --------------------- //
+                // Update Options Button //
+                // --------------------- //
+
+                {
+                    // TODO: Pass device data to the options button
+                }
+
+                // --------------------- //
+                // Update Offline Marker //
+                // --------------------- //
+
+                {
+                    const marker =
+                        this.root.shadowRoot.querySelector(`.offline-marker`);
+
+                    if (device.server.isOffline) marker.removeAttribute("hide");
+                    else marker.setAttribute("hide", "");
+                }
             },
         };
 
@@ -30,11 +90,78 @@ export default class PicowDeviceItem extends HTMLElement {
     }
 
     #render() {
+        this.classList.add("no-user-select");
+
         this.attachShadow({ mode: "open" });
         globalStylesToShadowRoot(this.shadowRoot);
 
-        // TODO: Continue here...
-        this.shadowRoot.innerHTML = html``;
+        this.shadowRoot.innerHTML = html`
+            <style>
+                :host {
+                    display: block;
+                    position: relative;
+                    border-radius: var(--ui-radius);
+                }
+
+                .current-color {
+                    position: absolute;
+                    top: var(--ui-spacing);
+                    right: var(--ui-spacing);
+                    bottom: var(--ui-spacing);
+                    left: var(--ui-spacing);
+
+                    border-radius: var(--ui-radius);
+
+                    box-shadow: 0 0 8px 1px var(--current-color, transparent);
+
+                    transition: box-shadow 0.35s linear;
+                }
+
+                .offline-marker {
+                    position: absolute;
+                    top: -0.25rem;
+                    left: 50%;
+
+                    color: var(--ui-destructive);
+
+                    transform: translateX(-50%);
+                }
+
+                .offline-marker[hide] {
+                    display: none;
+                }
+            </style>
+
+            <div class="current-color"></div>
+            <slot></slot>
+            <ui-secondary class="offline-marker"></ui-secondary>
+        `;
+
+        this.innerHTML = html`
+            <li class="is-card" style="cursor: pointer;">
+                <ui-label>
+                    <ui-flex-grid-row gap="0.25rem">
+                        <ui-flex-grid-item>
+                            <!-- TODO: I need a "PicowPowerIconButton" -->
+                            <device-item-power-button></device-item-power-button>
+                        </ui-flex-grid-item>
+
+                        <ui-flex-grid-item>
+                            <!-- TODO: I need a "PicowOptionsIconButton" -->
+                            <device-item-options></device-item-options>
+                        </ui-flex-grid-item>
+                    </ui-flex-grid-row>
+                </ui-label>
+            </li>
+        `;
+
+        /**
+         * @type {HTMLLIElement}
+         */
+        const card = this.querySelector("li.is-card");
+        card.onclick = async () => {
+            // TODO: Open a color picker dialog to select a color
+        };
 
         this.picow.set(this.device);
     }
---
+++ frontend/src/app/pages/devices-components/picow-power-button.js
@@ -0,0 +1,143 @@
+import { power as svgPower } from "ui/svg/smoothie-line-icons";
+import { html, UIIconButton } from "ui";
+import { utils } from "../../../lib";
+
+export default class PicowPowerButton extends UIIconButton {
+    /**
+     * @param {object} options
+     * @param {PicowStore} options.store
+     */
+    constructor({ store }) {
+        super();
+
+        this.store = store;
+
+        /**
+         * @type {Device | null}
+         */
+        this.device = null;
+
+        this.picow = {
+            root: this,
+
+            get state() {
+                // @ts-expect-error
+                return this.root.getAttribute("active");
+            },
+
+            /**
+             * @param {"pending" | "active" | null} state
+             */
+            set state(state) {
+                if (!state) {
+                    this.root.removeAttribute("state");
+                    return;
+                }
+
+                this.root.setAttribute("state", state);
+            },
+
+            /**
+             * @param {Device} device
+             */
+            set(device) {
+                this.root.device = device;
+                this.root.#handleButtonColor();
+            },
+
+            isOn() {
+                return !!this.root.device?.color?.find((n) => n > 0);
+            },
+        };
+
+        this.#render();
+    }
+
+    #render() {
+        this.ui.noripple = true;
+        this.ui.ghost = true;
+
+        this.shadowRoot.innerHTML += html`
+            <style>
+                :host {
+                    height: 100%;
+                    width: 3rem;
+                    color: black;
+                }
+
+                :host([state="active"]) {
+                    color: rgb(0, 255, 0);
+                }
+
+                :host([state="pending"]) {
+                    color: yellow;
+                }
+            </style>
+        `;
+
+        this.inneHTML = svgPower;
+
+        this.ui.events.on("click", async (ev) => {
+            ev.stopPropagation();
+            if (!this.device) return;
+
+            const prevStateBackup = this.picow.state;
+            this.picow.state = "pending";
+
+            // TODO: ...
+            const server = this.store.ui.get("server");
+            const addr = !server.port
+                ? server.host
+                : `${server.host}:${server.port}`;
+            const url = `${
+                server.ssl ? "https:" : "http:"
+            }//${addr}/device/color`;
+
+            try {
+                // TODO: Using the device color here, this field is currently missing
+                const color = this.picow.isOn()
+                    ? this.device.color.map(() => 0)
+                    : [255, 255, 255, 255];
+
+                const resp = await fetch(url, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        server: {
+                            addr: this.device.server.addr,
+                        },
+                        color,
+                    }),
+                });
+
+                if (resp.ok) {
+                    this.device.color = color;
+                } else {
+                    resp.text().then((r) => {
+                        const m = `Server response to ${url}: ${r}`;
+                        utils.throwAlert({ message: m, variant: "error" });
+                        console.error(m);
+                    });
+
+                    const m = `Fetch from ${url} with status code ${resp.status}`;
+                    console.error(m);
+                    utils.throwAlert({ message: m, variant: "error" });
+                }
+            } catch (err) {
+                utils.throwAlert({ message: err, variant: "error" });
+                console.error(err);
+            } finally {
+                this.picow.state = prevStateBackup;
+            }
+
+            this.#handleButtonColor();
+        });
+    }
+
+    #handleButtonColor() {
+        if (this.picow.isOn()) this.picow.state = "active";
+        else this.picow.state = null;
+    }
+}
+
+customElements.define("picow-power-button", PicowPowerButton);
