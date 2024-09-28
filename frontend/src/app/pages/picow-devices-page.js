2f606a1f8595389acc86ea5abed8149bfeead13b
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-28 05:08:42 +0200

    picow-devices-page: Handle events (appBar, store, websockets) and add some todos

--- frontend/src/app/pages/picow-devices-page.js
+++ frontend/src/app/pages/picow-devices-page.js
@@ -1,5 +1,5 @@
-import { CleanUp } from "ui";
-import { html, UIStackLayoutPage } from "ui";
+import { CleanUp, html, UIStackLayoutPage } from "ui";
+import { devicesEvents, utils } from "../../lib";
 
 export default class PicowDevicesPage extends UIStackLayoutPage {
     /**
@@ -35,23 +35,84 @@ export default class PicowDevicesPage extends UIStackLayoutPage {
     connectedCallback() {
         super.connectedCallback();
 
-        // -------------------- //
-        // Handle AppBar events //
-        // -------------------- //
-        // TODO: ...
-        // ------------------- //
-        // Handle Store events //
-        // ------------------- //
-        // TODO: ...
-        // ----------------------- //
-        // Handle WebSocket events //
-        // ----------------------- //
-        // TODO: ...
+        this.cleanup.add(
+            // -------------------- //
+            // Handle AppBar events //
+            // -------------------- //
+
+            this.appBar.events.on("add", () => {
+                // TODO: Create device setup dialog
+            }),
+
+            // ------------------- //
+            // Handle Store events //
+            // ------------------- //
+
+            this.store.ui.on("devices", (devices) => {
+                const list = this.querySelector("ul");
+                while (!!list.firstChild) list.removeChild(list.firstChild);
+                for (const device of devices) {
+                    // TODO: Create and append the device item
+                }
+            }),
+
+            // ----------------------- //
+            // Handle WebSocket events //
+            // ----------------------- //
+
+            devicesEvents.events.on("open", async () => {
+                try {
+                    this.store.ui.set("devices", await this.fetchApiDevices());
+                } catch (err) {
+                    utils.throwAlert({ message: err, variant: "error" });
+                }
+            }),
+
+            devicesEvents.events.on("message", async (devices) =>
+                this.store.ui.set("devices", devices)
+            )
+        );
+
+        this.fetchApiDevices()
+            .then((devices) => {
+                this.store.ui.set("devices", devices);
+            })
+            .catch((err) =>
+                utils.throwAlert({ message: err, variant: "error" })
+            );
     }
 
     disconnectedCallback() {
         super.disconnectedCallback();
         this.cleanup.run();
+    }
+
+    /**
+     * @private
+     * @returns {Promise<Device[]>}
+     */
+    async fetchApiDevices() {
+        const server = this.store.ui.get("server");
+        const addr = !server.port
+            ? server.host
+            : `${server.host}:${server.port}`;
+        const url = `${server.ssl ? "https:" : "http:"}//${addr}/api/devices`;
+
+        const resp = await fetch(url, { method: "GET" });
+        if (!resp.ok) {
+            resp.text().then((r) => {
+                const m = `Server response to ${url}: ${r}`;
+                utils.throwAlert({ message: m, variant: "error" });
+                console.error(m);
+            });
+
+            const m = `Fetch from "${url}" with status code ${resp.status}`;
+            utils.throwAlert({ message: m, variant: "error" });
+            console.error(m);
+            return;
+        }
+
+        return await resp.json();
     }
 }
 
