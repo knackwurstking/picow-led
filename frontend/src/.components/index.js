cbefdcca4368866449461124bdc68ad75dce6a1c
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-29 08:32:16 +0200

    Remove old files: "components/", "pages/"

--- frontend/src/.components/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./dialogs";
-export * from "./picow-app";
-export * from "./picow-app-bar";
-export * from "./picow-drawer";
--- frontend/src/.pages/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./devices-page";
-export * from "./settings-page";
