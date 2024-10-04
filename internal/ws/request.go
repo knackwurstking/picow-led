2c761d1f0cba050ac8e0f1ff8ec8d8a64ee1263b
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-04 19:55:58 +0200

    Add command type

--- internal/ws/request.go
+++ internal/ws/request.go
@@ -2,9 +2,17 @@ package ws
 
 import "encoding/json"
 
+const (
+	CommandGetApiDevices   = Command("GET api.devices")
+	CommandPostApiDevice   = Command("POST api.device")
+	CommandDeleteApiDevice = Command("DELETE api.device")
+)
+
+type Command string
+
 type Request struct {
 	Client  *Client `json:"-"`
-	Command string  `json:"command"`
+	Command Command `json:"command"`
 	Data    string  `json:"data"`
 }
 
