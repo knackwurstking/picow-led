e9270d9e86600623611c6ade0bf5c1aa8c6e6c67
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-04 11:53:22 +0200

    add `LoadFromPath` method to `Api`

--- pkg/picow/api.go
+++ pkg/picow/api.go
@@ -1,5 +1,12 @@
 package picow
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
 type Api struct {
 	Devices []*Device `json:"devices"`
 }
@@ -8,4 +15,35 @@ func NewApi() *Api {
 	return &Api{
 		Devices: make([]*Device, 0),
 	}
+}
+
+func (a *Api) LoadFromPath(path string) error {
+	i, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if i.IsDir() {
+		return fmt.Errorf("%s: destination hav to be a file", path)
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, a)
+}
+
+func (a *Api) SaveToPath(path string) error {
+	// TODO: Create dirs to path, strip last index "/" (file name)
+
+	// TODO: Marshal data and write to file
+
+	return nil
 }
