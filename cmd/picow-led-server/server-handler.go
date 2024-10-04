9db50b7170bfc018d4cd90a12e2329409effa7ab
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-04 07:34:00 +0200

    clean up main package

---
+++ cmd/picow-led-server/server-handler.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+type serverHandler struct{}
+
+func (*serverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Error("Recovered", "r", r)
+		}
+	}()
+
+	crw := &serverResponseWriter{
+		ResponseWriter: w,
+		Hijacker:       w.(http.Hijacker),
+	}
+
+	crw.Header().Set("Access-Control-Allow-Origin", "*")
+	crw.Header().Set("Content-Type", "text/plain")
+	http.DefaultServeMux.ServeHTTP(crw, r)
+
+	log := slog.Warn
+
+	if crw.status >= 200 && crw.status < 300 {
+		if crw.Header().Get("Content-Type") == "" {
+			crw.Header().Set("Content-Type", "application/json")
+		}
+
+		log = slog.Info
+	} else if crw.status >= 500 || crw.status == 0 {
+		log = slog.Error
+	}
+
+	log("Request",
+		"status", crw.status,
+		"addr", r.RemoteAddr,
+		"method", r.Method,
+		"url", r.URL,
+	)
+}
---
+++ cmd/picow-led-server/server-response-writer.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+type serverResponseWriter struct {
+	http.ResponseWriter
+	http.Hijacker
+	status int
+}
+
+func (crw *serverResponseWriter) WriteHeader(statusCode int) {
+	crw.status = statusCode
+	crw.ResponseWriter.WriteHeader(statusCode)
+}
