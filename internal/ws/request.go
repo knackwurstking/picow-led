c1738c0381ee9cb3a99e3aa2c1a1c2865f49eef0
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-05 19:55:55 +0200

    Move command stuff to separate file "command.go"

--- internal/ws/request.go
+++ internal/ws/request.go
@@ -2,17 +2,6 @@ package ws
 
 import "encoding/json"
 
-const (
-	CommandGetApiDevices      = Command("GET api.devices")
-	CommandPostApiDevice      = Command("POST api.device")
-	CommandPutApiDevice       = Command("PUT api.device")
-	CommandDeleteApiDevice    = Command("DELETE api.device")
-	CommandPostApiDevicePins  = Command("POST api.device.pins")
-	CommandPostApiDeviceColor = Command("POST api.device.color")
-)
-
-type Command string
-
 type Request struct {
 	Client  *Client `json:"-"`
 	Command Command `json:"command"`
