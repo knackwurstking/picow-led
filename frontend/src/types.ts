1f94d1e5449a3c4af2b8be6c128246b3bc5bd10f
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-09 12:13:31 +0200

    Add currentTheme to store

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,7 @@
 import type { UIStackLayout, UIStore } from "ui";
 
 import type { WSEvents_Device, WSEvents_Server } from "./lib/websocket";
+import type { UIThemeHandler_Theme } from "ui/src/ui-theme-handler/ui-theme-handler";
 
 export type PicowStackLayout = UIStackLayout<PicowStackLayout_Pages>;
 
@@ -17,4 +18,7 @@ export interface PicowStore_Events {
     devices: WSEvents_Device[];
     devicesColor: DevicesColor;
     server: WSEvents_Server;
+    currentTheme: {
+        theme: UIThemeHandler_Theme;
+    };
 }
