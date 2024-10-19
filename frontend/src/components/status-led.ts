c747fedaecb5762d7c45891c15a5cf8724d499fc
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-19 16:30:12 +0200

    Convert status-led to lit component

--- frontend/src/components/status-led.ts
+++ frontend/src/components/status-led.ts
@@ -1,55 +1,53 @@
-import { html } from "ui";
+import { css as CSS, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
 /**
- * HTML: `status-led`
+ * **Tag**: status-led
  *
- * Attributes:
- *   - __active__: *boolean*
+ * **Attributes**:
+ *  - active: `boolean`
  */
-export default class StatusLED extends HTMLElement {
-    constructor() {
-        super();
-        this.#renderStatusLED();
+@customElement("status-led")
+export class PicowStatusLED extends LitElement {
+    @property({ type: Boolean, attribute: "active", reflect: true })
+    active: boolean = false;
+
+    static get styles() {
+        return CSS`
+            :host {
+                display: block;
+            }
+
+            :host .outer {
+                padding: 0.25rem;
+                border-radius: 50%;
+                background-color: transparent;
+            }
+
+            :host .inner {
+                width: 1rem;
+                height: 1rem;
+                border-radius: 50%;
+                filter: blur(2px);
+
+                transition: background-color 0.15s linear;
+            }
+
+            :host(:not([active])) .inner {
+                background-color: rgb(255, 0, 0);
+            }
+
+            :host([active]) .inner {
+                background-color: rgb(0, 255, 0);
+            }
+        `;
     }
 
-    #renderStatusLED() {
-        this.attachShadow({ mode: "open" });
-        this.shadowRoot.innerHTML = html`
-            <style>
-                :host {
-                    display: block;
-                }
-
-                :host .outer {
-                    padding: 0.25rem;
-                    border-radius: 50%;
-                    background-color: var(--ui-bg);
-                }
-
-                :host .inner {
-                    width: 1rem;
-                    height: 1rem;
-                    border-radius: 50%;
-                    filter: blur(2px);
-
-                    transition: background-color 0.15s linear;
-                }
-
-                :host(:not([active])) .inner {
-                    background-color: rgb(255, 0, 0);
-                }
-
-                :host([active]) .inner {
-                    background-color: rgb(0, 255, 0);
-                }
-            </style>
-
+    protected render() {
+        return html`
             <div class="outer">
                 <div class="inner"></div>
             </div>
         `;
     }
 }
-
-console.debug(`Register the "status-led"`);
-customElements.define("status-led", StatusLED);
