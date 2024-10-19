38f184b9f63d039da629848b49d4c049a31d39d6
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-19 19:55:07 +0200

    Convert drawer to lit

--- frontend/src/app/picow-drawer.ts
+++ frontend/src/app/picow-drawer.ts
@@ -1,5 +1,105 @@
-import { LitElement } from "lit";
+import { html, LitElement, PropertyValues } from "lit";
 import { customElement } from "lit/decorators.js";
+import { UIButton, UIDrawer, UIStackLayout } from "ui";
+import { PicowStackLayoutPages } from "../types";
 
+/**
+ * **App**: picow-drawer
+ *
+ * **Public Methods**:
+ *  - `root()`
+ *  - `open()`
+ *  - `close()`
+ */
 @customElement("picow-drawer")
-export class PicowDrawer extends LitElement {}
+export class PicowDrawer extends LitElement {
+    private stackLayout(): UIStackLayout<PicowStackLayoutPages> {
+        return this.shadowRoot!.querySelector<
+            UIStackLayout<PicowStackLayoutPages>
+        >(`ui-stack-layout`)!;
+    }
+
+    protected render() {
+        return html`
+            <ui-drawer>
+                <ui-drawer-group nofold>
+                    <ui-drawer-group-item>
+                        <ui-button
+                            name="devices"
+                            style="justify-content: flex-start;"
+                            variant="ghost"
+                            color="primary"
+                        >
+                            Devices
+                        </ui-button>
+                    </ui-drawer-group-item>
+
+                    <ui-drawer-group-item>
+                        <ui-button
+                            name="groups"
+                            style="justify-content: flex-start;"
+                            variant="ghost"
+                            color="primary"
+                            disabled
+                        >
+                            Groups
+                        </ui-button>
+                    </ui-drawer-group-item>
+
+                    <ui-drawer-group-item>
+                        <ui-button
+                            name="scenes"
+                            style="justify-content: flex-start;"
+                            variant="ghost"
+                            color="primary"
+                            disabled
+                        >
+                            Scenes
+                        </ui-button>
+                    </ui-drawer-group-item>
+
+                    <ui-drawer-group-item>
+                        <ui-button
+                            name="settings"
+                            style="justify-content: flex-start;"
+                            variant="ghost"
+                            color="primary"
+                        >
+                            Settings
+                        </ui-button>
+                    </ui-drawer-group-item>
+                </ui-drawer-group>
+            </ui-drawer>
+        `;
+    }
+
+    protected updated(_changedProperties: PropertyValues): void {
+        const root = this.root();
+        const stackLayout = this.stackLayout();
+        let button: UIButton;
+
+        const pages: PicowStackLayoutPages[] = ["devices", "settings"];
+        for (const name of pages) {
+            button = this.shadowRoot!.querySelector(
+                `ui-button[name="devices"]`
+            )!;
+            button.onclick = () => {
+                stackLayout.clear();
+                stackLayout.set(name);
+                root.open = false;
+            };
+        }
+    }
+
+    public root(): UIDrawer {
+        return this.shadowRoot!.querySelector(`ui-drawer`)!;
+    }
+
+    public open() {
+        this.root().open = true;
+    }
+
+    public close() {
+        this.root().open = false;
+    }
+}
