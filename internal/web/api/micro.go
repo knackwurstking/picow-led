fd627dcf548965fa955dabe7e82803930417bac8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-25 00:09:54 +0200

    add more (micro) types

--- internal/web/api/micro.go
+++ internal/web/api/micro.go
@@ -1,10 +1,36 @@
 package api
 
+const (
+	// Micro Default IDs
+
+	MicroIDNoResponse = MicroID(-1)
+	MicroIDDefault    = MicroID(0)
+
+	// Micro Types
+
+	MicroTypeGET = MicroType("get")
+	MicroTypeSET = MicroType("set")
+
+	// Micro Groups
+
+	MicroGroupConfig = MicroGroup("config")
+	MicroGroupLED    = MicroGroup("led")
+
+	// MicroGroupInfo only used for `MicroTypeGET`
+	MicroGroupInfo = MicroGroup("led")
+)
+
+type (
+	MicroID    int
+	MicroType  string
+	MicroGroup string
+)
+
 type MicroRequest struct {
-	ID      int    `json:"id"`
-	Group   string `json:"group"`
-	Type    string `json:"type"`
-	Command string `json:"command"`
+	ID      MicroID    `json:"id"`
+	Group   MicroGroup `json:"group"`
+	Type    MicroType  `json:"type"`
+	Command string     `json:"command"`
 
 	// CommandArgs can be nil
 	// TODO: Maybe using generics here, just like before
@@ -12,8 +38,8 @@ type MicroRequest struct {
 }
 
 type MicroResponse struct {
-	ID    int    `json:"id"`
-	Error string `json:"error"`
+	ID    MicroID `json:"id"`
+	Error string  `json:"error"`
 
 	// Data contains the data requested, can be nil
 	// TODO: Maybe using generics here too, just like before
