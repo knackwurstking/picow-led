25a4457f2bfa79692b693aafe7e71e6ab9929430
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-25 00:33:39 +0200

    add some documentation

--- internal/web/api/micro.go
+++ internal/web/api/micro.go
@@ -27,21 +27,44 @@ type (
 )
 
 type MicroRequest struct {
-	ID      MicroID    `json:"id"`
-	Group   MicroGroup `json:"group"`
-	Type    MicroType  `json:"type"`
-	Command string     `json:"command"`
+	ID    MicroID    `json:"id"`
+	Group MicroGroup `json:"group"`
+	Type  MicroType  `json:"type"`
+
+	// Command mappings:
+	//
+	// Type: "set":
+	// 		- Group: "config" Command: "led"
+	// 		- Group: "led"    Command: "color"
+	//
+	// Type: "set":
+	// 		- Group: "config" Command: "led"
+	// 		- Group: "led"    Command: "color"
+	// 		- Group: "info"   Command: "temp"
+	// 		- Group: "info"   Command: "disk-usage"
+	// 		- Group: "info"   Command: "version"
+	Command string `json:"command"`
 
 	// CommandArgs can be nil
-	// TODO: Maybe using generics here, just like before
-	CommandArgs []any `json:"args"`
+	//
+	// 	if type is `MicroTypeSET` and group is `MicroGroupConfig`
+	//		if command is "led":
+	// 			[]uint8 - range between 0-28
+	//					  https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2024/02/Raspberry-Pi-Pico-W-RP2040-Rev3-Board-Pinout-GPIOs.png?quality=100&strip=all&ssl=1
+	//
+	// 	elif type is `MicroTypeSET` and group is `MicroGroupLED`
+	//		if command is "color":
+	// 			[]uint8 - range between 0-255
+	//
+	// 	else
+	// 		nil
+	CommandArgs []string `json:"args"`
 }
 
-type MicroResponse struct {
+type MicroResponse[T any | MicroPins | MicroColor | MicroTemp | MicroDiskUsage | MicroVersion] struct {
 	ID    MicroID `json:"id"`
 	Error string  `json:"error"`
 
-	// Data contains the data requested, can be nil
-	// TODO: Maybe using generics here too, just like before
-	Data any `json:"data"`
+	// Data contains the data requested
+	Data T `json:"data"`
 }
