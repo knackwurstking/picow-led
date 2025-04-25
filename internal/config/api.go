f1fdc852fe5c712f0e4e525ed5d1bc854a7a5f3e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-25 08:14:32 +0200

    add two servers to api options for testing

--- internal/config/api.go
+++ internal/config/api.go
@@ -3,12 +3,14 @@ package config
 import "picow-led/internal/api"
 
 func GetApiOptions(paths ...string) *api.Options {
-	// TODO: Get this api options from "~/.config/picow-led/api.yml" or
-	// 		 "api.yml" (use yaml v3)
+	// TODO: Get this api options from "~/.config/picow-led/api.yaml" or
+	// 		 "api.yaml" (use yaml v3)
 	o := &api.Options{}
 
 	o.Servers = []*api.Server{
-		// TODO: Add some servers here for testing
+		// NOTE: Just testing here, move this to "./api.yaml"
+		{Addr: "192.168.178.50:3000", Name: "Living Room"},
+		{Addr: "192.168.178.67:3000", Name: "Bed Room"},
 	}
 
 	return o
