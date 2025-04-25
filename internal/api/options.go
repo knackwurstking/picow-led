87af5b15cef032ff106e2b0985271546eb4b6423
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-25 08:14:20 +0200

    add name field for server type

--- internal/api/options.go
+++ internal/api/options.go
@@ -6,7 +6,9 @@ type Options struct {
 
 // Server contains host and port in use from a Device
 type Server struct {
-	Addr   string `json,yaml:"addr"`
+	Addr string `json,yaml:"addr"`
+	// Name could be empty (optional)
+	Name   string `json,yaml:"name"`
 	Online bool   `json:"online" yaml:"-"` //  Not used in configurations
 	Error  string `json:"error" yaml:"-"`  //  Not used in configurations
 }
