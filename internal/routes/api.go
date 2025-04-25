9d2be05fad0810a8bb2d6ee72bc6e397fc788e79
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-25 07:47:55 +0200

    add GET /api/devices route

--- internal/routes/api.go
+++ internal/routes/api.go
@@ -1,7 +1,8 @@
 package routes
 
 import (
-	"fmt"
+	"net/http"
+	"picow-led/internal/api"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,8 +10,6 @@ import (
 // apiDevices - GET - "/api/devices"
 func apiDevices(e *echo.Echo, data Options) {
 	e.GET(data.ServerPathPrefix+"/api/devices", func(c echo.Context) error {
-		// TODO: ...
-
-		return fmt.Errorf("under construction")
+		return c.JSON(http.StatusOK, api.GetDevices(data.Api))
 	})
 }
