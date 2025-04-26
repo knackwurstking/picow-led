e1bc4957cde46c218402c6d27f5364f9f8ca6dd3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-26 11:30:06 +0200

    remove logs

--- web/js/src/page-devices.ts
+++ web/js/src/page-devices.ts
@@ -1,14 +1,12 @@
 window.addEventListener("load", () => {
-    console.warn("window onload event");
     updateApiDevices();
 });
 
 window.addEventListener("focus", () => {
-    console.warn("window focus event");
     updateApiDevices();
 });
 
 function updateApiDevices() {
-    console.warn((window as any).api as Api);
     // TODO: Fetch devices from GET "/api/devices"
+    console.warn((window as any).api as Api);
 }
