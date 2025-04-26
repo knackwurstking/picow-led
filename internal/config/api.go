2187514b97bfe6574835afafb8c8951b072bdcb8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-26 05:43:09 +0200

    remove placeholder and update todo

--- internal/config/api.go
+++ internal/config/api.go
@@ -3,13 +3,11 @@ package config
 import "picow-led/internal/api"
 
 func GetApiOptions(paths ...string) *api.Options {
-	o := &api.Options{}
-
-	o.Servers = []*api.Server{
-		// TODO: Just testing here, move this to "./api.yaml"
-		{Addr: "192.168.178.50:3000", Name: "Living Room"},
-		{Addr: "192.168.178.67:3000", Name: "Bed Room"},
+	o := &api.Options{
+		Servers: []*api.Server{},
 	}
+
+	// TODO: Read configuration from path if possible
 
 	return o
 }
