2c3010501791360e5412429bbae19d1c8f42c75a
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-26 12:10:51 +0200

    clean up

--- web/js/src/api.ts
+++ web/js/src/api.ts
@@ -1,5 +1,19 @@
+interface Device {}
+
 class Api {
-    // TODO: GET "/api/devices"
+    private url(): string {
+        return `{{ .ServerPathPrefix }}`;
+    }
+
+    // NOTE: No need for this right now, i'm doing some server side rendering for now
+    //
+    //public async devices(): Promise<Device[]> {
+    //    const url = this.url() + "/api/devices";
+    //
+    //    // TODO: GET "/api/devices"
+    //
+    //    return [];
+    //}
 }
 
 (window as any).api = new Api();
--- web/js/src/page-devices.ts
+++ web/js/src/page-devices.ts
@@ -1,12 +1,1 @@
-window.addEventListener("load", () => {
-    updateApiDevices();
-});
 
-window.addEventListener("focus", () => {
-    updateApiDevices();
-});
-
-function updateApiDevices() {
-    // TODO: Fetch devices from GET "/api/devices"
-    console.warn((window as any).api as Api);
-}
