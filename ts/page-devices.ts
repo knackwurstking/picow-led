be87f0efd5153e3af47cef79bc7d89a00969ac62
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-27 08:43:10 +0200

    move ts file to .trash for now, will be removed later

--- ts/page-devices.ts
+++
@@ -1,7 +0,0 @@
-setOnlineIndicator("online");
-
-function setOnlineIndicator(state: "online" | "offline") {
-    const onlineIndicator =
-        document.querySelector<HTMLElement>(`.online-indicator`)!;
-    onlineIndicator.setAttribute(`data-state`, state);
-}
