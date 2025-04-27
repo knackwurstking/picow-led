8299134b3d617df9aba2954aee31ac573edab1c2
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-27 06:41:01 +0200

    clean up

--- ts/page-devices.ts
+++ ts/page-devices.ts
@@ -1,3 +1,7 @@
-const onlineIndicator =
-    document.querySelector<HTMLElement>(`.online-indicator`)!;
-onlineIndicator.setAttribute(`data-state`, "offline");
+setOnlineIndicator("online");
+
+function setOnlineIndicator(state: "online" | "offline") {
+    const onlineIndicator =
+        document.querySelector<HTMLElement>(`.online-indicator`)!;
+    onlineIndicator.setAttribute(`data-state`, state);
+}
