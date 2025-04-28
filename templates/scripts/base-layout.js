5522781490448915d03dc051b1cd4b2047edec6e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-28 21:13:57 +0200

    add base layout script

--- templates/scripts/base-layout.js
+++ templates/scripts/base-layout.js
@@ -1,2 +1,20 @@
-{{ define "script-base-layout" }}
-{{ end }}
+//{{ define "script-base-layout" }}
+(() => {
+    const utils = window.utils;
+
+    window.addEventListener("focus", async () => {
+        try {
+            const resp = await fetch("{{ .ServerPathPrefix }}/api/ping");
+            const data = await resp.text();
+            if (data === "pong") {
+                utils.setOnlineIndicator(true);
+            } else {
+                utils.setOnlineIndicator(false);
+            }
+        } catch (err) {
+            console.error(err);
+            utils.setOnlineIndicator(false);
+        }
+    });
+})();
+//{{ end }}
