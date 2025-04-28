08c306c2b783575cbc84dec67af3e6d723850eb8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-28 02:20:06 +0200

    put device list item into an extra function

--- ui/devices-page.go
+++ ui/devices-page.go
@@ -28,20 +28,7 @@ func DevicesPage(serverPathPrefix string, devices ...*api.Device) Node {
 			// Devices List
 			Span(
 				Class("ui-flex column gap align-center"),
-				Map(devices, func(d *api.Device) Node {
-					// Device
-					return Section(
-						Class("device-list-item ui-flex row gap justify-between align-center ui-padding"),
-						Style("width: 100%;"),
-						If(d.Server.Name != "", H4(Text(d.Server.Name))),
-						If(d.Server.Name == "", H4(Text(d.Server.Addr))),
-						Span(
-							Class("ui-flex-item"),
-							Style("flex: 0;"),
-							powerButton(d),
-						),
-					)
-				}),
+				Map(devices, deviceListItem),
 			),
 
 			// Templates for later
@@ -51,6 +38,20 @@ func DevicesPage(serverPathPrefix string, devices ...*api.Device) Node {
 			Script(
 				Raw("window.utils.setOnlineIndicator(true)"),
 			),
+		),
+	)
+}
+
+func deviceListItem(d *api.Device) Node {
+	return Section(
+		Class("device-list-item ui-flex row gap justify-between align-center ui-padding"),
+		Style("width: 100%;"),
+		If(d.Server.Name != "", H4(Text(d.Server.Name))),
+		If(d.Server.Name == "", H4(Text(d.Server.Addr))),
+		Span(
+			Class("ui-flex-item"),
+			Style("flex: 0;"),
+			powerButton(d),
 		),
 	)
 }
