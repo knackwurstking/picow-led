b347b7ed543d5fe59b9a5b4cda9250103b5af800
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-29 16:19:33 +0200

    fix focus triggered twice sometimes

--- templates/scripts/base-layout.js
+++ templates/scripts/base-layout.js
@@ -1,22 +1,38 @@
 //{{ define "script-base-layout" }}
 (() => {
-    /** @type {Utils} */
-    // @ts-ignore
-    const utils = window.utils;
+    /** @type {number | null} */
+    let timeout = null;
+    window.addEventListener("focus", () => {
+        if (timeout !== null) {
+            return;
+        }
 
-    window.addEventListener("focus", async () => {
-        try {
-            const resp = await fetch("{{ .ServerPathPrefix }}/api/ping");
-            const data = await resp.text();
-            if (data === "pong") {
-                utils.setOnlineIndicatorState(true);
-            } else {
+        // NOTE: This focus event gets triggered twice after tab switching,
+        //       this is just a workaround to prevent this
+        timeout = setTimeout(async () => {
+            /** @type {Utils} */
+            // @ts-ignore
+            const utils = window.utils;
+
+            try {
+                const resp = await fetch("{{ .ServerPathPrefix }}/api/ping");
+                const data = await resp.text();
+                if (data === "pong") {
+                    utils.setOnlineIndicatorState(true);
+                } else {
+                    utils.setOnlineIndicatorState(false);
+                }
+            } catch (err) {
+                console.error(err);
                 utils.setOnlineIndicatorState(false);
             }
-        } catch (err) {
-            console.error(err);
-            utils.setOnlineIndicatorState(false);
-        }
+
+            timeout = null;
+        }, 100);
+    });
+
+    window.addEventListener("load", () => {
+        window.dispatchEvent(new Event("focus"));
     });
 })();
 //{{ end }}
