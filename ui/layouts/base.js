8de3190815668dc801d6f6d2badd97074d9bbc77
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-02 08:55:35 +0200

    refactor: remove (() => { ... })()

--- ui/layouts/base.js
+++ ui/layouts/base.js
@@ -1,52 +1,50 @@
 //!{{ define "script-layout-base" }}
-(() => {
-    /** @type {import("../types.d.ts").PageWindow} */
-    // @ts-ignore
-    const w = window;
+/** @type {import("../types.d.ts").PageWindow} */
+// @ts-ignore
+const w = window;
 
-    /** @type {import("../types.d.ts").UIStore} */
-    const store = new w.ui.Store("picow-led:");
+/** @type {import("../types.d.ts").UIStore} */
+const store = new w.ui.Store("picow-led:");
 
-    /** @type {number | null} */
-    let timeout = null;
-    window.addEventListener("focus", () => {
-        if (timeout !== null) {
-            return;
-        }
+/** @type {number | null} */
+let timeout = null;
+window.addEventListener("focus", () => {
+    if (timeout !== null) {
+        return;
+    }
 
-        // NOTE: This focus event gets triggered twice after tab switching,
-        //       this is just a workaround to prevent this
-        timeout = setTimeout(async () => {
-            try {
-                const resp = await fetch("{{ .ServerPathPrefix }}/api/ping");
-                const data = await resp.text();
-                if (data === "pong") {
-                    w.utils.setOnlineIndicatorState(true);
+    // NOTE: This focus event gets triggered twice after tab switching,
+    //       this is just a workaround to prevent this
+    timeout = setTimeout(async () => {
+        try {
+            const resp = await fetch("{{ .ServerPathPrefix }}/api/ping");
+            const data = await resp.text();
+            if (data === "pong") {
+                w.utils.setOnlineIndicatorState(true);
 
-                    // NOTE: Can be removed if websockets are in use...
-                    // ...only reconnect to the websocket here
-                    updateStorage();
-                } else {
-                    w.utils.setOnlineIndicatorState(false);
-                }
-            } catch (err) {
-                console.error(err);
+                // NOTE: Can be removed if websockets are in use...
+                // ...only reconnect to the websocket here
+                updateStorage();
+            } else {
                 w.utils.setOnlineIndicatorState(false);
             }
+        } catch (err) {
+            console.error(err);
+            w.utils.setOnlineIndicatorState(false);
+        }
 
-            timeout = null;
-        }, 100);
+        timeout = null;
+    }, 100);
+});
+
+window.addEventListener("load", () => {
+    // Trigger the focus event once
+    window.dispatchEvent(new Event("focus"));
+});
+
+function updateStorage() {
+    w.api.devices().then((devices) => {
+        store.set("devices", devices);
     });
-
-    window.addEventListener("load", () => {
-        // Trigger the focus event once
-        window.dispatchEvent(new Event("focus"));
-    });
-
-    function updateStorage() {
-        w.api.devices().then((devices) => {
-            store.set("devices", devices);
-        });
-    }
-})();
+}
 //!{{ end }}
