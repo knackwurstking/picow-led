8b6a048cf607d4b694b82a11b395c9d670102bde
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-03 09:56:22 +0200

    update the service worker and get rid of some error loggings

--- ui/service-worker.js
+++ ui/service-worker.js
@@ -4,17 +4,35 @@ const CURRENT_CACHE = "picow-led-" + CACHE_VERSION;
 const cacheFiles = [
     process.env.SERVER_PATH_PREFIX + "/",
     process.env.SERVER_PATH_PREFIX + "/settings",
+
     process.env.SERVER_PATH_PREFIX + "/css/style.css",
     process.env.SERVER_PATH_PREFIX + "/css/ui-v4.1.0.css",
+
     process.env.SERVER_PATH_PREFIX + "/js/ui-v4.1.0.min.umd.cjs",
     process.env.SERVER_PATH_PREFIX + "/js/devices-address.js",
     process.env.SERVER_PATH_PREFIX + "/js/devices.js",
     process.env.SERVER_PATH_PREFIX + "/js/layout.js",
     process.env.SERVER_PATH_PREFIX + "/js/main.js",
     process.env.SERVER_PATH_PREFIX + "/js/settings.js",
+
+    process.env.SERVER_PATH_PREFIX + "/icons/apple-touch-icon-180x180.png",
+    process.env.SERVER_PATH_PREFIX + "/icons/favicon.ico",
+    process.env.SERVER_PATH_PREFIX + "/icons/icon.png",
+    process.env.SERVER_PATH_PREFIX + "/icons/pwa-192x192.png",
+    process.env.SERVER_PATH_PREFIX + "/icons/pwa-64x64.png",
+    process.env.SERVER_PATH_PREFIX + "/icons/maskable-icon-512x512.png",
+    process.env.SERVER_PATH_PREFIX + "/icons/pwa-512x512.png",
+
+    "https://fonts.googleapis.com/css2?family=Recursive:slnt,wght,CASL,CRSV,MONO@-15..0,300..1000,0..1,0..1,0..1&display=swap",
 ];
 
-const blackList = ["/api/.*"];
+// NOTE: This will be prefixed with ".*" and suffixed with "$"
+const blackList = [
+    "/api/.*",
+    "/screenshots/.*",
+    "service-worker.js",
+    "manifest.json",
+];
 
 self.addEventListener("activate", (evt) => {
     // @ts-expect-error
@@ -78,12 +96,6 @@ const update = async (
     /** @type {Request} */ request,
     /** @type {Response} */ response,
 ) => {
-    if (isBlackListed(request.url)) {
-        return new Promise(() =>
-            console.debug(`Nope, no caching for: ${request.url}`),
-        );
-    }
-
     const cache = await caches.open(CURRENT_CACHE);
     cache.put(request, response);
 };
@@ -96,12 +108,16 @@ self.addEventListener(
     "fetch",
     (/** @type {Event & { request: Request }} */ evt) => {
         if (!isBlackListed(evt.request.url)) {
+            console.debug(`fetch: caching: ${evt.request.url}`);
+
             // @ts-expect-error
             evt.respondWith(
                 fromNetwork(evt.request, 1e4).catch(() =>
                     fromCache(evt.request),
                 ),
             );
+        } else {
+            console.debug(`fetch: no caching for: ${evt.request.url}`);
         }
     },
 );
