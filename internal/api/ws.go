c3dfe15a6b666a5222eafe0f7bc3ed66deafe9d9
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-06 17:07:43 +0200

    WS, WSClient structs

--- internal/api/ws.go
+++ internal/api/ws.go
@@ -1,5 +1,24 @@
 package api
 
+import "sync"
+
 type WS struct {
-	// TODO: ...
+	clients []*WSClient
+
+	mutex *sync.Mutex
 }
+
+func NewWS() *WS {
+	return &WS{
+		mutex: &sync.Mutex{},
+	}
+}
+
+func (ws *WS) RegisterClient(c *WSClient) {
+	ws.mutex.Lock()
+	defer ws.mutex.Unlock()
+
+	ws.clients = append(ws.clients, c)
+}
+
+type WSClient struct{}
