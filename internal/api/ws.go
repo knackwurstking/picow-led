0f394526e57b45085d60d935765702f709dc25f5
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-06 15:17:41 +0200

    my next todos

---
+++ internal/api/ws.go
@@ -0,0 +1,5 @@
+package api
+
+type WS struct {
+	// TODO: ...
+}
---
+++ internal/routes/ws.go
@@ -0,0 +1,13 @@
+// API Routes:
+//   - TODO: GET - "/ws"
+package routes
+
+import "github.com/labstack/echo/v4"
+
+type WS struct {
+	ServerPathPrefix string
+}
+
+func wsRoutes(e *echo.Echo, o WS) {
+	// TODO: ...
+}
