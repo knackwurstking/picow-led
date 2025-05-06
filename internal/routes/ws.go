f51f7ed4b82816f20502c4ce27fbb8c1b2d9ba3f
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-06 17:08:04 +0200

    main ws route (readonly for clients)

--- internal/routes/ws.go
+++ internal/routes/ws.go
@@ -2,12 +2,28 @@
 //   - TODO: GET - "/ws"
 package routes
 
-import "github.com/labstack/echo/v4"
+import (
+	"picow-led/internal/api"
 
-type WS struct {
+	"github.com/labstack/echo/v4"
+	"golang.org/x/net/websocket"
+)
+
+type wsOptions struct {
 	ServerPathPrefix string
 }
 
-func wsRoutes(e *echo.Echo, o WS) {
-	// TODO: ...
+func wsRoutes(e *echo.Echo, ws *api.WS, o wsOptions) {
+	e.GET(o.ServerPathPrefix+"/ws", func(c echo.Context) error {
+		ws.RegisterClient(&api.WSClient{}) // TODO: ...
+
+		websocket.Handler(func(c *websocket.Conn) {
+			defer c.Close()
+			for {
+				// TODO: Keep alive loop here, this websocket is readonly
+			}
+		}).ServeHTTP(c.Response(), c.Request())
+
+		return nil
+	})
 }
