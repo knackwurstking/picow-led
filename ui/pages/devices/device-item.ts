67f7c3dd07f007d367e1af0b2966574ad4eb4157
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-11 15:38:36 +0200

    convert to typescript

--- ui/pages/devices/device-item.ts
+++ ui/pages/devices/device-item.ts
@@ -1,60 +1,63 @@
-/**
- * @param {Device} device
- * @param {(item: HTMLElement) => void|Promise<void>} onClickPowerButton
- * @returns {HTMLElement}
- */
-export function create(device, onClickPowerButton) {
-    /** @type {HTMLTemplateElement} */
-    const t = document.querySelector(`template[name="device-list-item"]`);
+type onClickPowerButton = (item: HTMLElement) => void | Promise<void>;
+
+export function create(
+    device: Device,
+    onClickPowerButton: onClickPowerButton,
+): HTMLElement {
+    const t = document.querySelector<HTMLTemplateElement>(
+        `template[name="device-list-item"]`,
+    );
     if (!t) {
         throw new Error(
             `Nope, template with name "color-range-slider" not found`,
         );
     }
 
-    /** @type {HTMLElement} */
-    const item = t.content
-        .cloneNode(true)
-        // @ts-expect-error
-        .querySelector(".device-list-item");
+    const item = (
+        t.content.cloneNode(true) as HTMLElement
+    ).querySelector<HTMLElement>(".device-list-item");
+    if (!item) throw new Error(`device list item is null`);
 
     return update(item, device, onClickPowerButton);
 }
 
-/**
- * @param {HTMLElement} item
- * @param {Device} device
- * @param {(item: HTMLElement) => void|Promise<void>} onClickPowerButton
- * @returns {HTMLElement}
- */
-export function update(item, device, onClickPowerButton) {
+export function update(
+    item: HTMLElement,
+    device: Device,
+    onClickPowerButton: onClickPowerButton,
+): HTMLElement {
     item.setAttribute("data-addr", device.server.addr);
 
-    /** @type {HTMLElement} */
-    const title = item.querySelector(`.title`);
+    const title = item.querySelector<HTMLElement>(`.title`);
+    if (!title) throw new Error(`title element is null`);
+
     title.innerHTML = device.server.name || device.server.addr;
 
-    /** @type {HTMLElement} */
-    const editButton = item.querySelector(`button.edit`);
+    const editButton = item.querySelector<HTMLElement>(`button.edit`);
+    if (!editButton) throw new Error(`edit button element is null`);
+
     editButton.setAttribute("data-addr", device.server.addr);
 
-    /** @type {HTMLButtonElement} */
-    const powerButton = item.querySelector(`button.power-button`);
+    const powerButton =
+        item.querySelector<HTMLButtonElement>(`button.power-button`);
+    if (!powerButton) throw new Error(`power button element is null`);
 
-    powerButton.onclick = async (
-        /** @type {MouseEvent & { currentTarget: HTMLButtonElement }} */ ev,
-    ) => {
-        if (ev.currentTarget.getAttribute("data-state") === "processing") {
+    powerButton.onclick = async (ev) => {
+        const target = ev.currentTarget as HTMLButtonElement;
+
+        if (target.getAttribute("data-state") === "processing") {
             return;
         }
-        ev.currentTarget.setAttribute("data-state", "processing");
+
+        target.setAttribute("data-state", "processing");
 
         await onClickPowerButton(item);
     };
 
-    // @ts-expect-error
-    powerButton.querySelector(`.background`).style.backgroundColor =
-        `rgb(${[...(device.color || []), 0, 0, 0].slice(0, 3).join(", ")})`;
+    const background = powerButton.querySelector<HTMLElement>(`.background`);
+    if (!background) throw new Error(`background element is null`);
+
+    background.style.backgroundColor = `rgb(${[...(device.color || []), 0, 0, 0].slice(0, 3).join(", ")})`;
 
     if (Math.max(...(device.color || []), 0, 0, 0)) {
         powerButton.setAttribute("data-state", "on");
