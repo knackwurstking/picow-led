98cc7c299eee7e25d4c39e362611f8b88a79a98c
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-13 15:50:38 +0200

    feat: ui: undo commit be2bff14

--- ui/pages/devices/address/main.ts
+++ ui/pages/devices/address/main.ts
@@ -76,18 +76,6 @@ async function setupColorStorage(colors: Colors): Promise<void> {
 
     const device = page.device();
 
-    // Append device color if not exists
-    if (Math.max(...(device.color || [])) > 0) {
-        const colorString = device.color!.join(colorStorageItem.colorSeparator);
-        if (
-            !colors.find(
-                (c) => c.join(colorStorageItem.colorSeparator) === colorString,
-            )
-        ) {
-            colors.push(device.color!);
-        }
-    }
-
     // Create color storage items
     colors.forEach((color, index) => {
         const item = colorStorageItem.create(index, color, {
