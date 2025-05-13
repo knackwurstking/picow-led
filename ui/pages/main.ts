177bcf5c0fc0dacb5d55bad12e4a5736fed5badc
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-13 20:18:22 +0200

    fix: ui: WebSocket (window) initialization

--- ui/pages/main.ts
+++ ui/pages/main.ts
@@ -1,9 +1,9 @@
 import { UIStore } from "../lib/store";
 import { Api } from "../lib/api";
 import * as utils from "../lib/window/utils";
-import * as ws from "../lib/window/ws";
+import { WS } from "../lib/ws";
 
 window.store = new UIStore();
 window.api = new Api();
 window.utils = utils.create();
-window.ws = ws.create();
+window.ws = new WS();
