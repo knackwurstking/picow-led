a6748034c9b1b6c47c1988dfd6fd0c4a1cc469ad
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-24 17:48:10 +0200

    remove stuff no longer needed for devices page

--- script/content/devices/main.ts
+++ script/content/devices/main.ts
@@ -1,30 +1,10 @@
-import * as deviceItem from "./device-item";
-
 document.addEventListener("DOMContentLoaded", async () => {
     setupAppBar();
 
-    window.store.listen("devices", (data) => {
-        const devices = data;
-        const devicesList = document.querySelector<HTMLElement>(
-            "._content.devices > .list",
-        )!;
-        devicesList.innerHTML = "";
-
-        devices.forEach((device) => {
-            const item = deviceItem.create(device, () => {
-                powerButtonToggle(device);
-            });
-
-            devicesList.appendChild(item);
-        });
-    });
-
-    await window.api.devices();
-    setTimeout(() => {
-        window.ws.events.addListener("open", async () => {
-            await window.api.devices();
-        });
-    });
+    // TODO: Power Button:
+    //  - query all device list item power buttons,
+    //  - handle data-state
+    //  - power button onclick event
 });
 
 async function setupAppBar() {
@@ -37,20 +17,20 @@ async function setupAppBar() {
     items["title"]!.innerText = "Devices";
 }
 
-async function powerButtonToggle(device: Device) {
-    let color: Color;
-    if (Math.max(...(device.color || [])) > 0) {
-        color = (device.pins || device.color || []).map(() => 0);
-    } else {
-        color = currentColorForDevice(device);
-    }
+//async function powerButtonToggle(device: Device) {
+//    let color: Color;
+//    if (Math.max(...(device.color || [])) > 0) {
+//        color = (device.pins || device.color || []).map(() => 0);
+//    } else {
+//        color = currentColorForDevice(device);
+//    }
+//
+//    await window.api.setDevicesColor(color, device);
+//}
 
-    await window.api.setDevicesColor(color, device);
-}
-
-function currentColorForDevice(device: Device): Color {
-    return (
-        window.store.currentDeviceColor(device.server.addr) ||
-        (device.pins || []).map(() => 255)
-    );
-}
+//function currentColorForDevice(device: Device): Color {
+//    return (
+//        window.store.currentDeviceColor(device.server.addr) ||
+//        (device.pins || []).map(() => 255)
+//    );
+//}
