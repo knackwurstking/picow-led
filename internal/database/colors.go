0e9500374815ca593df5bb768aff34be608545f8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-25 08:25:57 +0200

    create colors table

--- internal/database/colors.go
+++ internal/database/colors.go
@@ -10,6 +10,11 @@ func NewColors(db *sql.DB) (*Colors, error) {
 	// TODO: Insert default (colors) data (only if not exists)
 	_, err := db.Exec(`
 		CREATE TABLE IF NOT EXISTS colors (
+            "id" INTEGER NOT NULL,
+		    "r"  INTEGER NOT NULL,
+		    "g"  INTEGER NOT NULL,
+		    "b"  INTEGER NOT NULL,
+		    PRIMARY KEY("id" AUTOINCREMENT)
 		);
 	`)
 	if err != nil {
