1c0ff27345b7431691da6e6f0a06b21534958dcf
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-25 06:12:24 +0200

    remove note

--- internal/routes/register.go
+++ internal/routes/register.go
@@ -1,14 +1,3 @@
-// NOTE:
-//
-//  /api/devices
-//  /api/devices/:addr
-//  /api/devices/:addr/color
-//  /api/devices/:addr/pins
-//  /api/devices/:addr/name
-//  /api/devices/:addr/power
-//  /api/colors
-//  /api/colors/:index
-
 package routes
 
 import "github.com/labstack/echo/v4"
