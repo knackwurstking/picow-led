d7e27e7290cbfc4722cc2bc515dd50b6636c82a7
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-30 08:50:09 +0200

    handle /ws route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
+	"log/slog"
 	"picow-led/internal/database"
+	"picow-led/internal/ws"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/net/websocket"
 )
 
 type Options struct {
@@ -16,6 +19,38 @@ func Register(e *echo.Echo, o *Options) {
 	registerGroup(e.Group(o.ServerPathPrefix+"/api"), apiHandler)
 
 	// TODO: Register /ws routes
+	wsHandler := ws.NewHandler()
+
+	e.GET("/ws", func(c echo.Context) error {
+		websocket.Handler(func(conn *websocket.Conn) {
+			defer conn.Close()
+
+			client := ws.NewClient(conn)
+
+			wsHandler.Register(client)
+			defer wsHandler.Unregister(client)
+
+			for {
+				// Keep alive loop here, this websocket is readonly
+				message := ""
+
+				if err := websocket.Message.Receive(conn, &message); err != nil {
+					slog.Warn("Receive websocket message failed",
+						"error", err,
+						"RealIP", c.RealIP(),
+						"path", c.Request().URL.Path)
+					break
+				}
+
+				slog.Debug("Receive websocket message",
+					"message", message,
+					"RealIP", c.RealIP(),
+					"path", c.Request().URL.Path)
+			}
+		}).ServeHTTP(c.Response(), c.Request())
+
+		return nil
+	})
 }
 
 func registerGroup(g *echo.Group, apiHandler *APIHandler) {
