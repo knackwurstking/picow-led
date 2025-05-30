6b525ce4b99c2293472e06a72946c9745a455f2e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-30 08:52:32 +0200

    clean up

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -17,8 +17,36 @@ type Options struct {
 func Register(e *echo.Echo, o *Options) {
 	apiHandler := NewAPIHandler(o.DB)
 	registerGroup(e.Group(o.ServerPathPrefix+"/api"), apiHandler)
+	registerWebSocket(e)
+}
 
-	// TODO: Register /ws routes
+func registerGroup(g *echo.Group, apiHandler *APIHandler) {
+	g.GET("/devices", apiHandler.GetDevices)
+
+	g.GET("/devices/:addr", apiHandler.GetDevice)
+
+	g.GET("/devices/:addr/name", apiHandler.GetDeviceName)
+
+	g.GET("/devices/:addr/active_color", apiHandler.GetDeviceActiveColor)
+
+	g.GET("/devices/:addr/color", apiHandler.GetDeviceColor)
+	g.POST("/devices/:addr/color", apiHandler.PostDeviceColor)
+
+	g.GET("/devices/:addr/pins", apiHandler.GetDevicePins)
+
+	g.GET("/devices/:addr/power", apiHandler.GetDevicePower)
+	g.POST("/devices/:addr/power", apiHandler.PostDevicePower)
+
+	g.GET("/colors", apiHandler.GetColors)
+	g.POST("/colors", apiHandler.PostColors)
+	g.PUT("/colors", apiHandler.PutColors)
+
+	g.GET("/colors/:id", apiHandler.GetColorsID)
+	g.POST("/colors/:id", apiHandler.PostColorsID)
+	g.DELETE("/colors/:id", apiHandler.DeleteColorsID)
+}
+
+func registerWebSocket(e *echo.Echo) {
 	wsHandler := ws.NewHandler()
 
 	e.GET("/ws", func(c echo.Context) error {
@@ -51,30 +79,4 @@ func Register(e *echo.Echo, o *Options) {
 
 		return nil
 	})
-}
-
-func registerGroup(g *echo.Group, apiHandler *APIHandler) {
-	g.GET("/devices", apiHandler.GetDevices)
-
-	g.GET("/devices/:addr", apiHandler.GetDevice)
-
-	g.GET("/devices/:addr/name", apiHandler.GetDeviceName)
-
-	g.GET("/devices/:addr/active_color", apiHandler.GetDeviceActiveColor)
-
-	g.GET("/devices/:addr/color", apiHandler.GetDeviceColor)
-	g.POST("/devices/:addr/color", apiHandler.PostDeviceColor)
-
-	g.GET("/devices/:addr/pins", apiHandler.GetDevicePins)
-
-	g.GET("/devices/:addr/power", apiHandler.GetDevicePower)
-	g.POST("/devices/:addr/power", apiHandler.PostDevicePower)
-
-	g.GET("/colors", apiHandler.GetColors)
-	g.POST("/colors", apiHandler.PostColors)
-	g.PUT("/colors", apiHandler.PutColors)
-
-	g.GET("/colors/:id", apiHandler.GetColorsID)
-	g.POST("/colors/:id", apiHandler.PostColorsID)
-	g.DELETE("/colors/:id", apiHandler.DeleteColorsID)
 }
