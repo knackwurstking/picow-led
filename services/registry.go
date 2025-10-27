1c6299a072483a03515208a70fcf68e3e461d264
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-27 23:52:29 +0100

    feat: auto create table on NewRegistry

--- services/registry.go
+++ services/registry.go
@@ -11,11 +11,16 @@ type Registry struct {
 	DeviceSetups *DeviceSetups
 }
 
+// NewRegistry creates a new registry instance and will call the `CreateTables` method.
 func NewRegistry(db *sql.DB) *Registry {
 	r := &Registry{db: db}
 
 	r.Devices = NewDevices(r)
 	r.DeviceSetups = NewDeviceSetups(r)
+
+	if err := r.CreateTables(); err != nil {
+		panic("failed to create tables: " + err.Error())
+	}
 
 	return r
 }
