87f114119d1cf843b7f7722f7054651667db3913
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-27 22:08:21 +0100

    feat: add pins service

--- services/registry.go
+++ services/registry.go
@@ -8,12 +8,14 @@ type Registry struct {
 	db *sql.DB
 
 	Devices *Devices
+	Pins    *Pins
 }
 
 func NewRegistry(db *sql.DB) *Registry {
 	r := &Registry{db: db}
 
 	r.Devices = NewDevices(r)
+	r.Pins = NewPins(r)
 
 	return r
 }
