7864914d4038f24de67cace5952e5aef7a79ff3f
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-28 04:50:59 +0100

    feat: add bootstrap icon font libraries and serve assets dir

--- cmd/picow-led/server.go
+++ cmd/picow-led/server.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	picowled "github.com/knackwurstking/picow-led"
+	"github.com/knackwurstking/picow-led/env"
 	"github.com/knackwurstking/picow-led/handlers"
 	"github.com/knackwurstking/picow-led/services"
 	"github.com/labstack/echo/v4"
 )
 
 func router(e *echo.Echo, r *services.Registry) {
-	// TODO: Server static files first
+	e.StaticFS(env.Args.ServerPathPrefix+"/", picowled.GetAssets())
 
 	handlers.NewPages(r).Register(e)
 	handlers.NewHXHome(r).Register(e)
