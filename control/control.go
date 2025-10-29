d9a01e0d7d038ebddc001f924d0c892a79affeb6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-29 22:28:19 +0100

    wip(control): continue developing an idea

--- control/control.go
+++ control/control.go
@@ -2,6 +2,9 @@ package control
 
 import "github.com/knackwurstking/picow-led/models"
 
+// TODO: Define types for parsing picow (microcontroller) device responses,
+// and creating a request (JSON)
+
 // Pins
 
 func GetPins() (models.Pins, error)
@@ -9,6 +12,7 @@ func SetPins(pins models.Pins) error
 
 // Range
 
+// NOTE: Please verify the microcontroller repository first, as this feature might not be configurable per remote due to being hardcoded in the microcontroller.
 func GetRange() (min, max uint8, err error)
 func SetRange(min, max uint8) error
 
