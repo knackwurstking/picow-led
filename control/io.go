e40534dfc170a62bcd4ae17f2a17bfb05a8d99a9
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-31 00:30:31 +0100

    docs: update NewRequest doc

--- control/io.go
+++ control/io.go
@@ -32,14 +32,14 @@ type Request struct {
 //
 //	`TypeSet` ("set"):
 //		- group: "config" command: "pins"
-//	  	- group: "led"    command: "color"
+//		- group: "led"    command: "color"
 //
 //	`TypeGet` ("get"):
-//	 	- group: "config" command: "pins"
-//	  	- group: "led"    command: "color"
-//	  	- group: "info"   command: "temp"
-//	  	- group: "info"   command: "disk-usage"
-//	  	- group: "info"   command: "version"
+//		- group: "config" command: "pins"
+//		- group: "led"    command: "color"
+//		- group: "info"   command: "temp"
+//		- group: "info"   command: "disk-usage"
+//		- group: "info"   command: "version"
 //
 // Args (optional):
 //
@@ -47,10 +47,10 @@ type Request struct {
 //		[]uint8 - range between 0-28 converted to a slice with strings
 //			https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2024/02/Raspberry-Pi-Pico-W-RP2040-Rev3-Board-Pinout-GPIOs.png?quality=100&strip=all&ssl=1
 //
-//	elif `Type` is "set" and `Group` is `led and `Command` is "color":
+//	elif `Type` is "set" and `Group` is "led" and `Command` is "color":
 //		[]uint8 - range between 0-255 converted to a slice with strings
 //
-//	else
+//	else:
 //		nil
 //
 // Examples:
