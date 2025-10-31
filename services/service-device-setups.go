5689ac0cb61d675ae99551053746828cf07f0355
Udo Bauer <knackwurstking.tux@gmail.com>
2025-10-31 20:51:20 +0100

    feat(service-device-setups): Handle control pins after add/update pins

--- services/service-device-setups.go
+++ services/service-device-setups.go
@@ -3,19 +3,24 @@ package services
 import (
 	"log/slog"
 
+	"github.com/knackwurstking/picow-led/control"
 	"github.com/knackwurstking/picow-led/models"
 )
 
+// DeviceSetups struct manages the setup of devices.
 type DeviceSetups struct {
 	registry *Registry
 }
 
+// NewDeviceSetups creates a new instance of DeviceSetups with the provided registry.
 func NewDeviceSetups(registry *Registry) *DeviceSetups {
 	return &DeviceSetups{
 		registry: registry,
 	}
 }
 
+// CreateTable creates a table for device setup data if it doesn't already exist.
+// It returns an error if the database execution fails.
 func (p *DeviceSetups) CreateTable() error {
 	query := `CREATE TABLE IF NOT EXISTS device_setups (
 		device_id INTEGER UNIQUE NOT NULL,
@@ -26,8 +31,10 @@ func (p *DeviceSetups) CreateTable() error {
 	return err
 }
 
+// Get retrieves a device setup record from the database by device ID.
+// It returns a DeviceSetup instance and an error if the record is not found or execution fails.
 func (p *DeviceSetups) Get(id models.DeviceID) (*models.DeviceSetup, error) {
-	slog.Debug("Get device setup from database", "table", "device_setups", "device_id", id)
+	slog.Debug("Get device setup from database", "table", "device_setups", "id", id)
 
 	query := `SELECT * FROM device_setups WHERE device_id = ?`
 	devicesSetup, err := ScanDevicesSetup(p.registry.db.QueryRow(query, id))
@@ -38,8 +45,10 @@ func (p *DeviceSetups) Get(id models.DeviceID) (*models.DeviceSetup, error) {
 	return devicesSetup, nil
 }
 
+// List retrieves all device setup records from the database.
+// It returns a slice of DeviceSetup instances and an error if execution fails.
 func (p *DeviceSetups) List() ([]*models.DeviceSetup, error) {
-	slog.Debug("List device setup from database", "table", "device_setups")
+	slog.Debug("List device setups from database", "table", "device_setups")
 
 	query := `SELECT * FROM device_setups`
 	rows, err := p.registry.db.Query(query)
@@ -48,7 +57,7 @@ func (p *DeviceSetups) List() ([]*models.DeviceSetup, error) {
 	}
 	defer rows.Close()
 
-	var deviceSetups []*models.DeviceSetup
+	deviceSetups := make([]*models.DeviceSetup, 0)
 	for rows.Next() {
 		devicesSetup, err := ScanDevicesSetup(rows)
 		if err != nil {
@@ -60,6 +69,8 @@ func (p *DeviceSetups) List() ([]*models.DeviceSetup, error) {
 	return deviceSetups, nil
 }
 
+// Add inserts a new device setup record into the database.
+// It returns the inserted device ID and an error if execution fails.
 func (p *DeviceSetups) Add(deviceSetup *models.DeviceSetup) (models.DeviceID, error) {
 	slog.Debug("Add device setup to database", "table", "device_setups", "device_id", deviceSetup.DeviceID)
 
@@ -78,13 +89,13 @@ func (p *DeviceSetups) Add(deviceSetup *models.DeviceSetup) (models.DeviceID, er
 		return 0, HandleSqlError(err)
 	}
 
-	// TODO: Pins changed, using controls package to update the picow device
-
-	return models.DeviceID(id), nil
+	return models.DeviceID(id), p.controlPins(deviceSetup.DeviceID, deviceSetup.Pins)
 }
 
+// Update updates an existing device setup record in the database.
+// It returns an error if execution fails or the record is not found.
 func (p *DeviceSetups) Update(deviceSetup *models.DeviceSetup) error {
-	slog.Debug("Update device setup in database", "table", "device_setups", "device_id", deviceSetup.DeviceID)
+	slog.Debug("Update device setup in database", "table", "device_setups", "deviceSetup", deviceSetup)
 
 	if !deviceSetup.Validate() {
 		return ErrInvalidDeviceSetup
@@ -96,19 +107,30 @@ func (p *DeviceSetups) Update(deviceSetup *models.DeviceSetup) error {
 		return HandleSqlError(err)
 	}
 
-	// TODO: Pins changed, using controls package to update the picow device
-
-	return nil
+	return p.controlPins(deviceSetup.DeviceID, deviceSetup.Pins)
 }
 
+// Delete removes the record associated with a given device ID from the database.
+// It returns an error if the execution fails or if the device is not found.
 func (p *DeviceSetups) Delete(id models.DeviceID) error {
-	slog.Debug("Delete device setup from database", "table", "device_setups", "device_id", id)
+	slog.Debug("Delete device setup from database", "table", "device_setups", "id", id)
 
 	query := `DELETE FROM device_setups WHERE device_id = ?`
 	_, err := p.registry.db.Exec(query, id)
 	return HandleSqlError(err)
 }
 
+// controlPins updates the pins on the device using the control package.
+func (p *DeviceSetups) controlPins(deviceID models.DeviceID, pins []uint8) error {
+	device, err := p.registry.Devices.Get(deviceID)
+	if err != nil {
+		return err
+	}
+
+	return control.SetPins(device, pins...)
+}
+
+// ScanDevicesSetup scans a database row into a DeviceSetup instance.
 func ScanDevicesSetup(r Scannable) (*models.DeviceSetup, error) {
 	deviceSetup := &models.DeviceSetup{}
 
