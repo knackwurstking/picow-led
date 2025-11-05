3580405bfabe7ba664faf52fdd71daadb90402f2
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-05 05:32:39 +0100

    feat: scroll into view on toggle details

--- components/page-home_templ.go
+++ components/page-home_templ.go
@@ -200,7 +200,7 @@ func pageHome_section(id string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "\" ontoggle=\"event.currentTarget.scrollIntoView({behavior: 'smooth', block: 'start'})\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
