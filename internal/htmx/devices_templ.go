8a4f351266fd79d550b4e4ea163ea12ce18efc3b
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-12 04:27:15 +0100

    feat(htmx): implement device-item component with ID constant
    - Add IDDeviceItem constant for device item element IDs
    - Implement DeviceItem component with div wrapper and classes
    - Add fmt import for string formatting

--- internal/htmx/devices_templ.go
+++ internal/htmx/devices_templ.go
@@ -8,7 +8,14 @@ package htmx
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import "github.com/knackwurstking/picow-led/pkg/models"
+import (
+	"fmt"
+	"github.com/knackwurstking/picow-led/pkg/models"
+)
+
+const (
+	IDDeviceItem = "device-item-%d"
+)
 
 type DevicesProps struct {
 	Data []*models.Device
@@ -70,6 +77,23 @@ func DeviceItem(d *models.Device) templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var3 string
+		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf(IDDeviceItem, d.ID))
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/htmx/devices.templ`, Line: 29, Col: 42}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\" class=\"device-item bg-gray-100 p-4 rounded\"></div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		return nil
 	})
 }
