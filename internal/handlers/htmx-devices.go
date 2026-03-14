edded38cdd3bea4770bf420837c6a2891f48ea41
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-14 23:38:18 +0100

    Update TODO comment for color handling in device form

--- internal/handlers/htmx-devices.go
+++ internal/handlers/htmx-devices.go
@@ -134,7 +134,7 @@ func HTMXAddDeviceDialog(r *services.Registry, method string) echo.HandlerFunc {
 	return nil
 }
 
-// TODO: Add color to form data for get and post
+// TODO: Color handling, form value "color"
 func HTMXEditDeviceDialog(r *services.Registry, method string) echo.HandlerFunc {
 	parseForm := func(c echo.Context) (dialogs.EditDeviceFormData, []error) {
 		var errs []error
