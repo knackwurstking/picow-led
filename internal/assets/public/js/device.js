4b8da14006a6e816a318230d24bdc5218b18281c
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-17 19:09:55 +0100

    Add device color/white controls to UI and placeholder JS handlers
    This adds JavaScript stub functions for changing device colors and white
    channels, and wires them to the color picker and sliders in the device
    UI via onchange handlers. Also removes outdated WebSocket TODO comment.

---
+++ internal/assets/public/js/device.js
@@ -0,0 +1,14 @@
+function changeDeviceColor(addr, hex) {
+	// TODO: Change the device color via API call
+	console.debug(`Changing color of device ${addr} to`, hex);
+}
+
+function changeDeviceWhite(addr, value) {
+	// TODO: Change the device color via API call
+	console.debug(`Changing white of device ${addr} to`, value);
+}
+
+function changeDeviceWhite2(addr, value) {
+	// TODO: Change the device color via API call
+	console.debug(`Changing white2 of device ${addr} to`, value);
+}
