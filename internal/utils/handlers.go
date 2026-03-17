0805728a75ebe7b6545d1c1a1dc84319a46383bc
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-17 22:13:27 +0100

    Update error handling and add `ParseQueryColor` util

--- internal/utils/handlers.go
+++ internal/utils/handlers.go
@@ -1,33 +1,63 @@
 package utils
 
 import (
-	"fmt"
+	"errors"
 	"strconv"
+	"strings"
 
 	"github.com/knackwurstking/picow-led/pkg/models"
 	"github.com/labstack/echo/v4"
 )
 
+var (
+	ErrNotFound = errors.New("not found")
+	ErrInvalid  = errors.New("invalid value")
+)
+
 func ParseQueryID(c echo.Context) (models.ID, error) {
 	idStr := c.QueryParam("id")
 	if idStr == "" {
-		return 0, fmt.Errorf("missing device ID")
+		return 0, ErrNotFound
 	}
 	id, err := strconv.ParseInt(idStr, 10, 64)
 	if err != nil {
-		return 0, fmt.Errorf("invalid device ID: %v", err)
+		return 0, ErrInvalid
 	}
 	return models.ID(id), nil
+}
+
+func ParseQueryColor(c echo.Context) ([]uint8, error) {
+	colorStr := c.QueryParam("color")
+	if colorStr == "" {
+		return nil, ErrNotFound
+	}
+
+	var color []uint8
+	colorStrSplit := strings.Split(colorStr, ",")
+	for _, part := range colorStrSplit {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			color = append(color, 0)
+			continue
+		}
+		c, err := strconv.ParseUint(part, 10, 8)
+		if err != nil {
+			return nil, ErrInvalid
+		}
+		color = append(color, uint8(c))
+	}
+
+	return color, nil
 }
 
 func ParseParamID(c echo.Context) (models.ID, error) {
 	idStr := c.Param("id")
 	if idStr == "" {
-		return 0, fmt.Errorf("missing device ID")
+		return 0, ErrNotFound
 	}
 	id, err := strconv.ParseInt(idStr, 10, 64)
 	if err != nil {
-		return 0, fmt.Errorf("invalid device ID: %v", err)
+		return 0, ErrInvalid
 	}
 	return models.ID(id), nil
 }
