8f905a3a5fb782abf75f58439bf49de4d4a0db90
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-17 19:32:21 +0100

    Add ParseParamID util

--- internal/utils/handlers.go
+++ internal/utils/handlers.go
@@ -19,3 +19,15 @@ func ParseQueryID(c echo.Context) (models.ID, error) {
 	}
 	return models.ID(id), nil
 }
+
+func ParseParamID(c echo.Context) (models.ID, error) {
+	idStr := c.Param("id")
+	if idStr == "" {
+		return 0, fmt.Errorf("missing device ID")
+	}
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid device ID: %v", err)
+	}
+	return models.ID(id), nil
+}
