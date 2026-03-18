2ae077948d8c04dad0a1eb215777b58452aad651
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-18 02:27:53 +0100

    refactor: improve console.debug messages for device color changes
    Update log messages to include 'for' and 'with' for better grammar when
    logging device color and white value changes.

--- internal/assets/public/js/device.js
+++ internal/assets/public/js/device.js
@@ -1,14 +1,14 @@
 function changeDeviceColor(addr, hex) {
 	// TODO: Change the device color via API call
-	console.debug(`Changing color of device ${addr} to`, hex);
+	console.debug(`Changing color for device with ${addr} to`, hex);
 }
 
 function changeDeviceWhite(addr, value) {
 	// TODO: Change the device color via API call
-	console.debug(`Changing white of device ${addr} to`, value);
+	console.debug(`Changing white for device with ${addr} to`, value);
 }
 
 function changeDeviceWhite2(addr, value) {
 	// TODO: Change the device color via API call
-	console.debug(`Changing white2 of device ${addr} to`, value);
+	console.debug(`Changing white2 for device with ${addr} to`, value);
 }
