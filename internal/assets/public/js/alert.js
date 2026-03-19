f7bace17cc9ceb8c3b87ff1990174a5f48cb88f1
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-19 13:09:02 +0100

    Refactor Alert component, change into AddAlert script function (templ)

--- internal/assets/public/js/alert.js
+++ internal/assets/public/js/alert.js
@@ -1,7 +1,5 @@
 (function() {
-	var alertContainer = document.querySelectorAll(`[data-alert-container]`)
-
-	alertContainer.forEach(container => {
+	document.querySelectorAll(`[data-alert-container]`).forEach(container => {
 		// Watch this container if anything new is added to it
 		new MutationObserver(mutations => {
 			mutations.forEach(mutation => {
@@ -16,45 +14,4 @@
 			})
 		}).observe(container, { childList: true })
 	})
-
-	// Global add alert function
-	window.addAlert = function(containerID, message, type = "info") {
-		console.debug(`Adding alert to container ${containerID}: ${message} (${type})`)
-
-		// Create a new alert element
-		const alert = document.createElement("div")
-		alert.setAttribute("data-alert", "")
-		alert.setAttribute("data-alert-type", type)
-		alert.textContent = message
-
-		switch (type) {
-			case "error":
-				alert.className = "px-4 py-2 rounded mb-2 cursor-pointer transition-opacity duration-300 bg-red-500 text-red-100"
-				break
-			case "warning":
-				alert.className = "px-4 py-2 rounded mb-2 cursor-pointer transition-opacity duration-300 bg-yellow-500 text-yellow-100"
-				break
-			case "info":
-				alert.className = "px-4 py-2 rounded mb-2 cursor-pointer transition-opacity duration-300 bg-blue-500 text-blue-100"
-				break
-			default:
-				alert.className = "px-4 py-2 rounded mb-2 cursor-pointer transition-opacity duration-300 bg-gray-500 text-gray-100"
-		}
-
-		// Add click handler to remove the alert on click
-		alert.addEventListener("click", () => {
-			alert.remove()
-		})
-
-		// Append the alert to the first alert container
-		if (alertContainer.length > 0) {
-			for (var container of alertContainer) {
-				if (container.id === containerID) {
-					container.appendChild(alert)
-				}
-			}
-		} else {
-			console.warn("No alert container found. Please add an element with [data-alert-container] attribute.")
-		}
-	}
 })();
