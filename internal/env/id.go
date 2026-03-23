df4684999303d99ac222e4bedda1e82e999d0429
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-23 07:48:51 +0100

    Move ids to env package

--- internal/env/id.go
+++ internal/env/id.go
@@ -1,5 +1,13 @@
 package env
 
 const (
+	// Layout: alert container
 	IDAlertContainer = "alert-container"
+
+	// Home: devices tab
+	IDDeviceItem        = "device-item-%d"
+	IDPowerToggleSwitch = "power-toggle-switch-%d"
+
+	// Home: groups tab
+	IDGroupItem = "group-item-%d"
 )
