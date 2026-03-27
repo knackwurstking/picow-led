03d1756dc43967dc7c473cb87bba8781d6219595
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-27 08:11:31 +0100

    Add TODO: passing pins to the device page, or use a separate HTMX handler for this

--- internal/handlers/pages.go
+++ internal/handlers/pages.go
@@ -21,7 +21,7 @@ func Home(c echo.Context) error {
 
 func Device(r *services.Registry, method string) echo.HandlerFunc {
 	render := func(c echo.Context, device *models.Device) error {
-		t := pages.Device(device)
+		t := pages.Device(device) // TODO: Get and pass pins, or use a separate HTMX endpoint for that?
 		if err := t.Render(c.Request().Context(), c.Response()); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Errorf("Failed to render page: %w", err))
 		}
