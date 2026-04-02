319840de35fa756151a4b2b9737eaf6340e820fd
Udo Bauer <knackwurstking.tux@gmail.com>
2026-04-02 10:24:21 +0200

    Add server path prefix to the script path (templui utils)

--- internal/utils/templui.go
+++ internal/utils/templui.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/a-h/templ"
+	"github.com/knackwurstking/picow-led/internal/env"
 	"github.com/templui/templui/components"
 
 	twmerge "github.com/Oudwins/tailwind-merge-go"
@@ -81,7 +82,7 @@ var ScriptURL = func(path string) string {
 
 // componentScriptBasePath is the base public path for component JavaScript files.
 // In the import workflow this stays "/templui/js". The CLI rewrites it to the user's local jsPublicPath.
-var componentScriptBasePath = "/js/templui"
+var componentScriptBasePath = env.ServerPathPrefix + "/js/templui"
 
 // UseUnminifiedScripts switches component script loading to the unminified files.
 // Leave this false in normal use and set it to true during app startup for debugging.
